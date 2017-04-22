cf629305cc6f041101419c85ab877b001ad4d1f8
Wasssssss <wleead@ust.hk>
2017-04-23 03:29:15 +0800

    putpoints

--- js/path.js
+++ js/path.js
@@ -1,6 +1,8 @@
 var directionsDisplay;
 var directionsService;
 var map;
+var InfoWindow123 = new google.maps.InfoWindow();;
+var service;
 var routeboxer = null;
 var distance = 0.01; // km
 var points = [];
@@ -41,9 +43,9 @@ function initMap() {
         //     directionsDisplay.setDirections(result);
         // }
         if (status == 'OK') {
-            
+            points = [];
             console.log(result);
-            var colors = ["red","blue","#a4f4ef"];
+            var colors = ["red","blue","#8c8810"];
             for (var i = 0, len = result.routes.length; i < len; i++) {
                 new google.maps.DirectionsRenderer({
                     map: map,
@@ -60,21 +62,78 @@ function initMap() {
                 infowindow.setContent(result.routes[i].legs[0].distance.text+"<br>"+result.routes[i].legs[0].duration.text+"<br>"+colors[i]);
                 infowindow.setPosition(result.routes[i].legs[0].steps[parseInt(result.routes[i].legs[0].steps.length*(i+1)/(result.routes.length+1))].end_location);
                 infowindow.open(map);
-                points.push(result.route[i]);
+                for (var j = 0; j< result.routes[i].overview_path.length/10; j++){
+                    console.log(j);
 
+                   points.push(result.routes[i].overview_path[j*10]);
+                }
             }
+            console.log(points);
         } else {
             $("#error").append("Unable to retrieve your route<br />");
         }
     });
+    
     var trafficLayer = new google.maps.TrafficLayer();
     trafficLayer.setMap(map);
     
 
-
 }
 
+function putPoints(con){
+    var request1;
+    service = new google.maps.places.PlacesService(map);
+    for(var i = con; i < points.length; i++){
+        // var marker = new google.maps.Marker({
+        // map: map,
+        // position: points[i]
+        // });
+        request1 = {
+            location: points[i],
+            radius: 300,
+            types: ['restaurant','cafe','shopping_mall']
+        }
+        console.log("yo");
+        service.nearbySearch(request1,callback);
+        if(i%10 == 0){
+            sleep(1000);
+            return putPoints(con+10);
+        }
+    }
+}
 
+function callback(results, status){
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+        console.log(results);
+        console.log("yo2");
+        for (var i = 0; i < results.length; i++) {
+          var place = results[i];
+          createMarker(results[i]);
+        }
+    }else{
+
+        console.log(status);
+    }
+}
+
+function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+    });
+
+
+
+}
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
 angular.module('path-app', ['firebase'])
     .controller('pathCtrl', ['$scope', '$firebaseObject', '$firebaseArray', '$window', function ($scope, $firebaseObject, $firebaseArray, $window) {
 
