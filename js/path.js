2b8dcf326cb25924d038eab9dcce8d8f5fd84539
Vivian Lee <vivianhtlee@gmail.com>
2017-04-22 17:06:11 +0800

    add path-app

---
+++ js/path.js
@@ -0,0 +1,4 @@
+angular.module('path-app', ['firebase'])
+    .controller('pathCtrl', ['$scope', '$firebaseObject', '$firebaseArray', '$window', function ($scope, $firebaseObject, $firebaseArray, $window) {
+
+    }]);
